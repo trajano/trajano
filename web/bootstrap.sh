9a89fece63b346cb2067c4473e2401c39a5b6aac
Archimedes Trajano <developer@trajano.net>
2018-02-03 18:54:56 -0500

    custom web

--- web/bootstrap.sh
+++ web/bootstrap.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 if [ ! -e /etc/letsencrypt/live ]
 then
-  certbot certonly -q --standalone --email arch@trajano.net --agree-tos --rsa-key-size 4096 -d trajano.net -d www.trajano.net -d i.trajano.net -d gw.trajano.net -d site.trajano.net -d ms.trajano.net
+  certbot -n -q certonly --standalone --email arch@trajano.net --agree-tos --rsa-key-size 4096 -d trajano.net -d www.trajano.net -d i.trajano.net -d gw.trajano.net -d site.trajano.net -d ms.trajano.net
 fi
 exec nginx -g "daemon off;"
