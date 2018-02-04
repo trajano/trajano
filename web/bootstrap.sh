4f8534fd615fa5d8b88bbb1c38ff9a9138e888dd
Archimedes Trajano <developer@trajano.net>
2018-02-03 20:13:41 -0500

    added registry but to i.

--- web/bootstrap.sh
+++ web/bootstrap.sh
@@ -2,5 +2,6 @@
 if [ ! -e /etc/letsencrypt/live ]
 then
   certbot -n -q certonly --standalone --email arch@trajano.net --agree-tos --rsa-key-size 4096 -d trajano.net -d www.trajano.net -d i.trajano.net -d gw.trajano.net -d site.trajano.net -d ms.trajano.net
+  openssl dhparam -out /etc/letsencrypt/dhparams.pem 2048 > /dev/null
 fi
 exec nginx -g "daemon off;"
