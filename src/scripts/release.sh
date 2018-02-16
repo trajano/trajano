424343257839b07d4bcf7d8ba4c173d1f1a473ff
Archimedes Trajano <arch@trajano.net>
2018-02-16 16:09:23 -0500

    Added release process
    Fixes #17
    
    Utilizes a script to perform the release process and the trigger would
    be from git.

---
+++ src/scripts/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e -x
+logmsg=$(git log --format=%B -n 1 HEAD)
+case "$logmsg" in
+  [maven-release-plugin]*)
+    ;;
+  *)
+    git checkout -B release
+    git reset --hard HEAD
+    mvn -B release:prepare
+    mvn release:perform
+    git checkout -B master 
+    mvn scm:checkin -Dmessage="merge from release"
+    ;;
+esac
