36d87354ca566cf888d8c094d74084b388e9ddc3
Archimedes Trajano <developer@trajano.net>
2018-02-18 09:06:21 -0500

    Attempt for prepare

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -2,6 +2,8 @@
 logmsg=$(git log --format=%B -n 1 HEAD)
 if [[ "$logmsg" != "[maven-release-plugin]"* ]]
 then
+  git checkout release
   git status
   git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+  mvn release:prepare
 fi
