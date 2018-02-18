625d805a6dbb85a6b67bc89dc535c318cd19f48f
Archimedes Trajano <developer@trajano.net>
2018-02-18 00:17:44 -0500

    Use travis stages

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,14 +1,7 @@
 #!/bin/bash -e -x
 logmsg=$(git log --format=%B -n 1 HEAD)
-case "$logmsg" in
-  [maven-release-plugin]*)
-    ;;
-  *)
-    git checkout -B release
-    git reset --hard HEAD
-    mvn -B release:prepare
-    mvn release:perform
-    git checkout -B master 
-    mvn scm:checkin -Dmessage="merge from release"
-    ;;
-esac
+if [[ "$logmsg" != "[maven-release-plugin]"* ]]
+then
+  git status
+  git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+fi
