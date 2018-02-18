f4147a4bf10e93613c5756ce1fe314250cfbe509
Archimedes Trajano <developer@trajano.net>
2018-02-18 00:17:44 -0500

    Use travis stages
    This is a rework on how the release process is going to performed in the
    future,

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -e -x
 logmsg=$(git log --format=%B -n 1 HEAD)
-case "$logmsg" in
-  [maven-release-plugin]*)
-    ;;
-  *)
-    git checkout -B release
-    git reset --hard HEAD
-    mvn -B release:prepare
-    mvn release:perform
-    git checkout -B master 
-    mvn scm:checkin -Dmessage="merge from release"
-    ;;
-esac
+if [[ "$logmsg" == "[maven-release-plugin] prepare for next development iteration" ]]
+then
+  git fetch origin master
+  git checkout FETCH_HEAD -B master
+  git merge --no-ff release
+  git status
+  git push https://${SONAR_GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master
+  git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+elif [[ "$logmsg" != "[maven-release-plugin]"* ]]
+then
+  git checkout release
+  git status
+  git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+  mvn release:prepare
+  mvn release:perform
+fi
