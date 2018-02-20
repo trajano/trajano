9a3acd186a304693d58862e8509e9ebb374119f1
Archimedes Trajano <arch@trajano.net>
2018-02-20 10:45:11 -0500

    Used push to repository url

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -6,8 +6,7 @@ then
   git checkout FETCH_HEAD -B master
   git merge --no-ff release
   git status
-  git remote set-url --push origin https://${SONAR_GITHUB_TOKEN}@github.com/${TRAVIS_SLUG}.git
-  git push origin master
+  git push https://${SONAR_GITHUB_TOKEN}@github.com/${TRAVIS_SLUG}.git master
   git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 elif [[ "$logmsg" != "[maven-release-plugin]"* ]]
 then
