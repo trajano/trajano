1202c621132a3f293d55b4d481be2de023a6aa89
Archimedes Trajano <developer@trajano.net>
2018-04-12 00:56:05 -0400

    x

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,2 @@
-docker-compose build && docker-compose push 
+docker-compose build intranet web && docker-compose push intranet web
 docker stack deploy -c docker-compose.yml --prune --with-registry-auth trajano
